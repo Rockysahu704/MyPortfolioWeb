31d26c472de9bb9a4f57507b7fb200149376d2ea
Rocky Sahu <sahurocky704@gmail.com>
2026-08-28 11:51:45 +0530

    Fix media file serving

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -46,11 +48,23 @@ urlpatterns = [
     include("apps.contact.urls")
     ),
 
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        }
+    ),
+
   
 ]
 
 
-urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+# urlpatterns += static(
+#         settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT
+#     )
+
+ # Serve media files
+
+
