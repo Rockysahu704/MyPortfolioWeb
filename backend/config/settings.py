eee228dd8fd22abcefb51753d274368e1518bc6f
Rocky Sahu <sahurocky704@gmail.com>
2026-08-30 18:18:20 +0530

    cros origin fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,15 +41,24 @@ ALLOWED_HOSTS = config(
     default="localhost,127.0.0.1"
 ).split(",")
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
 
-CORS_ALLOWED_ORIGINS = config(
-    "CORS_ALLOWED_ORIGINS",
-    default="http://localhost:5173,http://127.0.0.1:5173, https://rocky-sahu-portfolio.vercel.app"
-).split(",")
+
+# CORS_ALLOWED_ORIGINS = config(
+#     "CORS_ALLOWED_ORIGINS",
+#     default="http://localhost:5173,http://127.0.0.1:5173,https://rocky-sahu-portfolio.vercel.app"
+# ).split(",")
+
+CORS_ALLOWED_ORIGINS = [
+    origin.strip()
+    for origin in config(
+        "CORS_ALLOWED_ORIGINS",
+        default=(
+            "http://localhost:5173,"
+            "http://127.0.0.1:5173,"
+            "https://rocky-sahu-portfolio.vercel.app"
+        )
+    ).split(",")
+]
 
 
 # Application definition
