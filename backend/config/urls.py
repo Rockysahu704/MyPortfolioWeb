9752d91756d8116229759101b8db89fad41aa895
Rocky Sahu <sahurocky704@gmail.com>
2026-08-30 14:36:07 +0530

    media url set for local

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.views.static import serve
-from django.urls import re_path
+from django.conf.urls.static import static
+# from django.views.static import serve
+# from django.urls import re_path
 # from django.conf.urls.static import static
 
 urlpatterns = [
@@ -54,23 +55,27 @@ urlpatterns = [
         include("apps.social.urls")
     ),
 
-    re_path(
-        r'^media/(?P<path>.*)$',
-        serve,
-        {
-            'document_root': settings.MEDIA_ROOT,
-        }
-    ),
+    # re_path(
+    #     r'^media/(?P<path>.*)$',
+    #     serve,
+    #     {
+    #         'document_root': settings.MEDIA_ROOT,
+    #     }
+    # ),
 
   
 ]
 
 
-# urlpatterns += static(
-#         settings.MEDIA_URL,
-#         document_root=settings.MEDIA_ROOT
-#     )
 
- # Serve media files
+
+# Serve media files only during local development
+
+if settings.DEBUG:
+
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 
 
