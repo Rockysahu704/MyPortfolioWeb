04e713b2cc84fba51c37dd819d7334e0b5db503d
Rocky Sahu <sahurocky704@gmail.com>
2026-09-03 10:40:00 +0530

    cors origin 3000 added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -53,6 +53,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://rocky-sahu-portfolio.vercel.app",
     "https://rita-portfolio-three.vercel.app",
+    "http://localhost:3000",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
